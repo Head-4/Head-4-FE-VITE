3b2fe4116426c409a14ccbed3057e7dae1189dc0
Hina0222 <hina0222@naver.com>
2025-03-19 18:28:41 +0900

    fix: 에러 메세지 수정

--- src/pages/utilPages/ErrorPage.tsx
+++ src/pages/utilPages/ErrorPage.tsx
@@ -1,23 +1,21 @@
-import { useRouteError } from 'react-router-dom';
+import {useRouteError} from 'react-router-dom';
+import Typography from "../../components/Typography.tsx";
 
 export const ErrorPage = () => {
-    const error: unknown = useRouteError();
+  const error: unknown = useRouteError();
 
-    if (error instanceof Error) {
-        console.error(error.message);
-        return (
-            <div>
-                <h1>에러!!</h1>
-                <p>
-                    <i>{error.message}</i>
-                </p>
-            </div>
-        );
-    }
-
+  if (error instanceof Error) {
+    console.error(error.message);
     return (
-        <div>
-            <h1>알 수 없는 에러가 발생했습니다!</h1>
-        </div>
+      <div>
+        <Typography typoSize="H4">크롬이나 사파리 브라우저를 열어 접속해주세요!</Typography>
+      </div>
     );
+  }
+
+  return (
+    <div>
+      <Typography typoSize="H4">알 수 없는 에러가 발생했습니다.</Typography>
+    </div>
+  );
 };
